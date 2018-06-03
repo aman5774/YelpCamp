0581bb1db7633d54e0da5d290beaafa65c28834b
Aman Bajpai <amanbajpai120@gmail.com>
2018-06-03 18:57:20 +0530

    add campground author

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -16,11 +16,15 @@ router.get("/", function(req, res){
 });
 
 // CREATE - creates the new campground
-router.post("/", function(req , res){
+router.post("/", isLoggedIn, function(req , res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = {name:name, image:image, description:description};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name:name, image:image, description:description, author:author};
     
     Campground.create(newCampground , function(err, newlyCreated){
         if(err){
@@ -32,7 +36,7 @@ router.post("/", function(req , res){
 });
 
 //NEW - show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
     res.render("campgrounds/new");
 });
 
@@ -47,5 +51,13 @@ router.get("/:id",function(req, res){
         }
     });
 });
+
+//middleware for checking user is logged In or not
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
